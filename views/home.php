3ef60e227efbc0b010418cfb10c4f0dd6fda00cd
Nikoolaii <nikolailemerre@gmail.com>
2023-02-01 11:54:08 +0100

    Change home message

--- views/home.php
+++ views/home.php
@@ -7,27 +7,36 @@
       <img src="/assets/logo.svg" class="h-32 group-hover:-rotate-12 group-focus-visible:-rotate-12 transition-transform duration-300">
       <h1 class="font-bold text-blue-500 text-7xl font-bannerFont hidden lg:block">MarieTeam</h1>
     </div>
-  </div>
-  <div class="w-full lg:h-auto text-center">
-    <h1 class="text-2xl font-bold text-blue-500 m-4">Qui est MarieTeam ?</h1>
-    <p class="font-bannerFont m-4 text-xl italic">"Des trajets d'exception avec une équipe d'exception.."</p>
-    <p class="p-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptates iusto deleniti voluptatum similique assumenda esse quod veniam eligendi quidem illum obcaecati fugiat molestiae nobis nam! Eum impedit illum sit debitis ad, ex dolorum corporis delectus, earum vel iure rerum necessitatibus voluptate illo aut quibusdam aliquam saepe libero esse hic similique eius praesentium aspernatur. Ab totam delectus saepe consequuntur praesentium, dolorum nemo aperiam, nesciunt ipsa placeat tempora blanditiis temporibus enim consequatur fuga nobis eum aspernatur culpa quaerat accusamus quia veniam rem distinctio. Unde repellat voluptates tenetur ipsam omnis magnam. Adipisci, maxime dolor ut quas sit, minima voluptas deserunt iusto, autem fugit neque dolorum explicabo reprehenderit mollitia accusamus est molestias temporibus. Alias ducimus repellat ratione mollitia illum, reiciendis dolores voluptas maxime molestias consequatur quos quis dolorem at voluptate? Provident aliquam totam reprehenderit asperiores perspiciatis facere, quibusdam ad pariatur corporis vel dolorem commodi itaque autem voluptates, ducimus odio ea nemo tenetur sunt rerum. Suscipit odit cum, mollitia, itaque, cumque voluptatibus in eligendi quia quam rem officia repudiandae! Sint dolorem, enim suscipit, minima dolorum reiciendis tenetur possimus saepe dolor maiores modi? Rem eaque id voluptate in enim hic quam voluptatibus iure consectetur non modi, est maiores sint ratione, eveniet fuga ipsam quidem commodi cum? Molestias temporibus sint magni ab beatae porro a officia debitis ipsa modi maxime voluptatibus odit explicabo error, numquam tenetur? Dolorem eveniet magni veritatis officia porro nostrum maiores, non cupiditate ea ipsam inventore minima veniam ab laborum voluptatum recusandae assumenda dolorum quidem atque unde nam! Architecto, distinctio. Sapiente, cumque eaque!</p>
-  </div>
-  <div class="ile w-full text-center mb-4">
-    <h1 class="text-2xl font-bold text-blue-500">Les Destinations</h1>
-    <div class="flex items-center justify-center flex-col xl:flex-row text-2xl font-bold text-blue-500">
-      <div>
-        <img src="/assets/Destination/iledehouat.jpeg" class="w-80 lg:w-96 h-52 lg:h-64 rounded-xl m-7">
-        <p class="font-bannerFont">Ile-de-Houat</p>
-      </div>
-      <div>
-        <img src="/assets/Destination/belleile.jpeg" class="w-80 lg:w-96 h-52 lg:h-64 rounded-xl m-7">
-        <p class="font-bannerFont">Belle île</p>
-      </div>
-      <div>
-        <img src="/assets/Destination/iledere.jpeg" class="w-80 lg:w-96 h-52 lg:h-64 rounded-xl m-7">
-        <p class="font-bannerFont">île de Ré</p>
-      </div>
+    <div class="w-full lg:h-auto text-center">
+        <h1 class="text-2xl font-bold text-blue-500 m-4">Qui est MarieTeam ?</h1>
+        <p class="font-bannerFont m-4 text-xl italic">"Des trajets d'exception avec une équipe d'exception.."</p>
+        <p class="p-6">MarieTeam est une compagnie maritime moderne et efficace, fondée pour répondre aux besoins en matière de transport maritime des entreprises et des particuliers. Avec une flotte de navires hautement performants et une équipe dédiée de professionnels passionnés, MarieTeam offre un service de qualité supérieure à ses clients.
+
+            <br />MarieTeam se concentre sur la sécurité et la fiabilité de ses opérations. Tous les navires de la compagnie sont constamment mis à jour avec les équipements les plus récents et les plus avancés pour garantir un voyage en toute sécurité. Les équipages sont formés à la sécurité en mer et sont prêts à faire face à toutes les situations d'urgence.
+
+            <br />MarieTeam offre une variété de services de transport maritime, y compris le transport de marchandises, le transport de passagers, les opérations de chargement et de déchargement, ainsi que le transport de fret par conteneurs. La compagnie travaille en étroite collaboration avec ses clients pour comprendre leurs besoins spécifiques et leur offrir des solutions adaptées à leurs besoins.
+
+            <br />Le personnel de MarieTeam est constitué d'experts en logistique et en transport maritime, qui utilisent leur expertise pour garantir une livraison rapide et efficace des marchandises. La compagnie est fière de son engagement en faveur de l'environnement et de son utilisation de technologies écologiques pour minimiser son impact sur l'environnement.
+
+            <br />MarieTeam est une compagnie maritime en pleine croissance et se consacre à offrir un service de qualité supérieure à ses clients. La compagnie est déterminée à établir des relations durables avec ses clients et à leur offrir un service fiable et abordable. Si vous êtes à la recherche d'une compagnie maritime de confiance pour vos besoins de transport, MarieTeam est le choix parfait pour vous.
+        </p>
+    </div>
+    <div class="ile w-full text-center mb-4">
+        <h1 class="text-2xl font-bold text-blue-500">Les Destinations</h1>
+        <div class="flex items-center justify-center flex-col xl:flex-row text-2xl font-bold text-blue-500">
+            <div>
+                <img src="/assets/Destination/iledehouat.jpeg" class="w-80 lg:w-96 h-52 lg:h-64 rounded-xl m-7">
+                <p class="font-bannerFont">Ile-de-Houat</p>
+            </div>
+            <div>
+                <img src="/assets/Destination/belleile.jpeg" class="w-80 lg:w-96 h-52 lg:h-64 rounded-xl m-7">
+                <p class="font-bannerFont">Belle île</p>
+            </div>
+            <div>
+                <img src="/assets/Destination/iledere.jpeg" class="w-80 lg:w-96 h-52 lg:h-64 rounded-xl m-7">
+                <p class="font-bannerFont">île de Ré</p>
+            </div>
+        </div>
     </div>
   </div>
 </div>
